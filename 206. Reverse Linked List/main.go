97b37b414b62a26fcf0fd0a70a18bb72279dd7d3
Kenshi <james85529@gmail.com>
2023-11-08 15:23:15 +0800

    206. reversed linked list

---
+++ 206. Reverse Linked List/main.go
@@ -0,0 +1,27 @@
+package main
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseList(head *ListNode) *ListNode {
+	if head == nil {
+		return head
+	}
+	var prev *ListNode
+	curr := head
+
+	for curr.Next != nil {
+		next := curr.Next
+		curr.Next = prev
+		prev = curr
+		curr = next
+	}
+	curr.Next = prev
+
+	return curr
+
+}
