155ca23db70d207d8ad6f440a17390e4aca5a3ed
Kenshi <james85529@gmail.com>
2024-01-09 18:05:51 +0800

    105: build binary tree from preorder and inorder list

---
+++ 105. Construct Binary Tree from Preorder and Inorder Traversal/README.md
@@ -0,0 +1,5 @@
+## 105. Construct Binary Tree from Preorder and Inorder Traversal
+
+- First value in the preorder array is guaranteed to be the value of the root node.
+- Use the root node to seperate left and right subtrees in the inorder array.
+- do this recursively
---
+++ 105. Construct Binary Tree from Preorder and Inorder Traversal/construct-binary-tree-from-preorder-and-inorder-traversal.go
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func buildTree(preorder []int, inorder []int) *TreeNode {
+	if len(preorder) == 0 || len(inorder) == 0 {
+		return nil
+	}
+	root := &TreeNode{Val: preorder[0]}
+	mid := findIndex(inorder, preorder[0])
+
+	root.Left = buildTree(preorder[1:mid+1], inorder[:mid])
+	root.Right = buildTree(preorder[mid+1:], inorder[mid+1:])
+
+	return root
+}
+
+func findIndex(slice []int, target int) int {
+	for i, v := range slice {
+		if v == target {
+			return i
+		}
+	}
+
+	return -1
+}
